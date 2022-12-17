4ccf725e0db7771bc750bab8194702ef0270a9f8
Titov Dmitry <dm.a.titov@yandex.ru>
2022-12-17 12:32:15 +0300

    begin the new way

---
+++ chess/chess.py
@@ -0,0 +1,6 @@
+class Chess:
+    def __init__(self):
+        pass
+
+    def play(self):
+        pass
---
+++ main.py
@@ -0,0 +1,10 @@
+from chess import Chess
+
+
+def main():
+    chess_instance = Chess()
+    chess_instance.play()
+
+
+if __name__ == "__main__":
+    main()
