3fb4db61aa931d9ab9402a1977b237ec1323c107
Titov Dmitry <dm.a.titov@yandex.ru>
2022-12-18 02:21:39 +0300

    fix if in Pawn.get_moves(): check next position

--- chess/pieces.py
+++ chess/pieces.py
@@ -17,7 +17,7 @@ class Pawn(Piece):
         if position.y >= 8:
             return []
         next_position = position + Position(0, 1)
-        if BoardBehaviour.at(board_access, next_position) != Empty:
+        if board_access[next_position] != 0:
             return []
         return [Move(position, next_position)]
 
